791db049fbe8e857f23172d6b8695a700f1cb8df
Helga <maduwubh@tcd.ie>
2026-07-07 20:29:53 +0100

    implimented Player class

---
+++ models/player.py
@@ -0,0 +1,13 @@
+
+
+class Player:
+    def __init__(self, name, score=0):
+        self.name = name
+        self.has_played = False
+
+    def mask_as_played(self):
+        self.has_played = True
+        
+        
+    def __str__(self):
+        return self.name
